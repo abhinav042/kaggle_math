55613311e41dc82d0d89724c1fe2a877518a8762
Abhinav Goyal <abhinav.goyal@live.com>
2017-12-30 09:05:46 -0500

    init proj

---
+++ titanic/titanic.py
@@ -0,0 +1,41 @@
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# get the dataframe from csv
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+
+combine = [train_df, test_df]
+
+# previewing the data
+# print(train_df.head())
+# print(train_df.tail())
+
+# finding out the data-types, completeness of the features 
+# print(train_df.info())
+# print(test_df.info())
+
+# describing the data
+# print(train_df.describe())
+
+# classifying 
+train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+train_df[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False)
